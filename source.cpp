ec998297dacaed7655581f8fcaa0db84fe5e3242
vRogue-Development <105680416+vRogue-Development@users.noreply.github.com>
2023-06-06 01:53:13 -0700

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -6,7 +6,6 @@ static __int64 rebase(__int64 address, __int64 base = 0x1E9DE1A0000)
 	return (address - base + reinterpret_cast<__int64>(GetModuleHandle(0)));
 }
 
-static auto print = reinterpret_cast<void(__cdecl*)(__int64, const char*, ...)>(rebase(0x1E9DF15D8A0));
 static auto get_task_scheduler = reinterpret_cast<__int64(__cdecl*)()>(rebase(0x1E9DEA3EA40));
 
 void initialize()
